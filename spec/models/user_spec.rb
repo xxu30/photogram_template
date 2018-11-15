012fc6d8ab0edd156a344786d82839ef54c1727f
Raghu Betina <raghu@firstdraft.com>
2018-11-15 01:34:34 +0000

    Validate uniqueness of users.username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe User, type: :model do
 
     describe "Validations" do
 
+    it { should validate_uniqueness_of(:username) }
+
     it { should validate_presence_of(:username) }
       
     end
