a0a1b0c708bd7f06242aebbdf5fb68050f062347
Raghu Betina <raghu@firstdraft.com>
2018-11-15 01:34:34 +0000

    Validate presence of users.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :username, :presence => true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe User, type: :model do
     end
 
     describe "Validations" do
+
+    it { should validate_presence_of(:username) }
       
     end
 end
