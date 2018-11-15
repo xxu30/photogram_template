7bcea3bbc7d105aa78881bf8a671ea8d026eb918
Raghu Betina <raghu@firstdraft.com>
2018-11-15 01:35:15 +0000

    Indirect association between users and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,14 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :followers,
+             :through => :received_follow_requests,
+             :source => :sender
+
+  has_many   :following,
+             :through => :sent_follow_requests,
+             :source => :recipient
+
   has_many   :liked_photos,
              :through => :likes,
              :source => :photo
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,10 @@ RSpec.describe User, type: :model do
 
     describe "InDirect Associations" do
 
+    it { should have_many(:followers) }
+
+    it { should have_many(:following) }
+
     it { should have_many(:liked_photos) }
 
     it { should have_many(:feed) }
