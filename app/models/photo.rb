2a4209c2337b670cc688c9e5a326001d3fb4e4d1
Raghu Betina <raghu@firstdraft.com>
2018-11-15 01:34:34 +0000

    Validate presence of photos.image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,8 @@ class Photo < ApplicationRecord
 
   # Validations
 
+  validates :image, :presence => true
+
   validates :owner_id, :presence => true
 
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Photo, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:image) }
+
     it { should validate_presence_of(:owner_id) }
       
     end
