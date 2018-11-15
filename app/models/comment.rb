ff39bc07f4a7ad0226d46bac20879f63e6e417e5
Raghu Betina <raghu@firstdraft.com>
2018-11-15 01:34:34 +0000

    Validate presence of comments.body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,8 @@ class Comment < ApplicationRecord
 
   # Validations
 
+  validates :body, :presence => true
+
   validates :commenter_id, :presence => true
 
   validates :photo_id, :presence => true
