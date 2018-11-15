d074ac6240682fd1c7bf64224624246e6791dcb1
Raghu Betina <raghu@firstdraft.com>
2018-11-15 01:37:27 +0000

    Only follow_request user can access show page

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,5 +1,5 @@
 class FollowRequestsController < ApplicationController
-  before_action :current_user_must_be_follow_request_sender, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_follow_request_sender, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_follow_request_sender
     follow_request = FollowRequest.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
