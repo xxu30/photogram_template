1ce6dbbefc018d7ca3235c678abb4e56aee512b2
Raghu Betina <raghu@firstdraft.com>
2018-11-15 01:37:26 +0000

    Only like user can modify like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,14 @@
 class LikesController < ApplicationController
+  before_action :current_user_must_be_like_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_like_user
+    like = Like.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == like.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @likes = Like.all
 
