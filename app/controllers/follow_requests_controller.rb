c3adc3ba6cd972f05af9da80d36bbed37f8af92b
Raghu Betina <raghu@firstdraft.com>
2018-11-15 01:37:27 +0000

    Only follow_request user can modify follow_request

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,4 +1,14 @@
 class FollowRequestsController < ApplicationController
+  before_action :current_user_must_be_follow_request_sender, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_follow_request_sender
+    follow_request = FollowRequest.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == follow_request.sender
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @follow_requests = FollowRequest.all
 
